11d2a517e560e650c47efd6b8ef91ed0fc278fd1
Asyraf <asyrafirfan@todak.com>
2023-08-30 17:38:41 +0800

    Disable test features

--- tests/Feature/ExampleFeatureTest.php
+++ tests/Feature/ExampleFeatureTest.php
@@ -7,55 +7,68 @@ use Tests\FeatureTestCase;
 
 class ExampleFeatureTest extends FeatureTestCase
 {
-    /**
-     * Throw 200 if valid role to create user
-     *
-     * @test
-     *
-     * @dataProvider validRolesToCreateUserProvider
-     *  */
-    public function user_can_be_created_by_valid_roles(RoleType $role): void
-    {
-        $this->asRole($role);
-
-        $response = $this->postJson('/users', [
-            'name' => '::name::',
-            'email' => '::name::@test.com',
-        ]);
-
-        $response->assertSuccessful();
-    }
 
     /**
-     * Throw 403 if invalid role to create user
+     * A basic test example.
      *
      * @test
-     *
-     * @dataProvider invalidRolesToCreateUserProvider
-     *  */
-    public function user_cannot_be_created_by_invalid_roles(RoleType $role): void
+     */
+    public function example()
     {
-        $this->asRole($role);
+        $response = $this->get('/');
 
-        $response = $this->postJson('/users', [
-            'name' => '::name::',
-            'email' => '::name::@test.com',
-        ]);
-
-        $response->assertStatus(403);
+        $response->assertStatus(200);
     }
 
-    public static function validRolesToCreateUserProvider(): array
-    {
-        return [
-            [RoleType::Admin],
-        ];
-    }
+    // /**
+    //  * Throw 200 if valid role to create user
+    //  *
+    //  * @test
+    //  *
+    //  * @dataProvider validRolesToCreateUserProvider
+    //  *  */
+    // public function user_can_be_created_by_valid_roles(RoleType $role): void
+    // {
+    //     $this->asRole($role);
 
-    public static function invalidRolesToCreateUserProvider(): array
-    {
-        return [
-            [RoleType::Staff],
-        ];
-    }
+    //     $response = $this->postJson('/users', [
+    //         'name' => '::name::',
+    //         'email' => '::name::@test.com',
+    //     ]);
+
+    //     $response->assertSuccessful();
+    // }
+
+    // /**
+    //  * Throw 403 if invalid role to create user
+    //  *
+    //  * @test
+    //  *
+    //  * @dataProvider invalidRolesToCreateUserProvider
+    //  *  */
+    // public function user_cannot_be_created_by_invalid_roles(RoleType $role): void
+    // {
+    //     $this->asRole($role);
+
+    //     $response = $this->postJson('/users', [
+    //         'name' => '::name::',
+    //         'email' => '::name::@test.com',
+    //     ]);
+
+    //     $response->assertStatus(403);
+    // }
+
+    // public static function validRolesToCreateUserProvider(): array
+    // {
+    //     return [
+    //         [RoleType::Admin],
+    //     ];
+    // }
+
+    // public static function invalidRolesToCreateUserProvider(): array
+    // {
+    //     return [
+    //         [RoleType::Staff],
+    //     ];
+    // }
 }
